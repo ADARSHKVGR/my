b90735f957c2819ddb951f6deb5a673f305783eb
adarsh <kvadarsh91@gmai.com>
2022-09-27 12:52:25 +0530

    commit

---
+++ bin/sample.dart
@@ -0,0 +1,10 @@
+void main() {
+  //variable declaration
+  //syntax : datatype  variablename = value;
+
+  String name = "Arun";
+  int age = 20;
+  //string interpolation
+  print("my name is $name");
+  print(" i am $age yrs old");
+}
